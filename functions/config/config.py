da265fbc8a23c6a46f8f30f25f1223f77e3e0d04
Arsalan Bashir <arsalan.b4@gmail.com>
2019-07-27 17:27:40 +0400

    V1

--- functions/config/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-current_dir = os.path.dirname(os.path.realpath(__file__))
-
-SCOPES = ['https://www.googleapis.com/auth/analytics openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile']
-R_URI = "http://localhost:5000/v1/connect/webhook/ga_integration"
-SECRET = current_dir + "/secret.json"
--- functions/datapoints.py
+++ functions/datapoints.py
@@ -8,28 +8,26 @@ import requests
 
 def get_stats(args):
     try:
-        domain_name = args['service_auth']['domain_name']
-        api_key = args['service_auth']['api_key']
+        domain_name = args["service_auth"]["domain_name"]
+        api_key = args["service_auth"]["api_key"]
 
         request_url = "https://api.mailgun.net/v3/%s/stats/total" % domain_name
-        event = [args['datapoint']]
+        event = [args["datapoint"]]
 
-        response = requests.get(request_url, auth=("api", api_key),
-                                params={
-            "event": event,
-            "duration": "1m"
-        })
+        response = requests.get(
+            request_url,
+            auth=("api", api_key),
+            params={"event": event, "duration": "1m"},
+        )
         if str(response.status_code) != "200":
-            message = json.loads(response._content)['message']
-            return {'status': 'failure', 'error': message}
+            message = json.loads(response._content)["message"]
+            return {"status": "failure", "error": message}
         returned = json.loads(response._content)
-        if args['datapoint'] == "failed":
-            first_value = returned['stats'][0][event[0]]['permanent']['total']
+        if args["datapoint"] == "failed":
+            first_value = returned["stats"][0][event[0]]["permanent"]["total"]
         else:
-            first_value = returned['stats'][0][event[0]]['total']
+            first_value = returned["stats"][0][event[0]]["total"]
 
-        return {'status': 'success', 'data': {
-            'returned_val': str(first_value)
-        }}
-    except Exception as error:
-        return {'status': 'failure', 'error': 'unexpected error'}
+        return {"status": "success", "data": {"returned_val": str(first_value)}}
+    except:
+        return {"status": "failure", "error": "unexpected error"}
